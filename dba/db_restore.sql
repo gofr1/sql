5f3faf33ad131974d7f2bdee100fa9743703ef41
Ilia Odintsov <gofr.one@gmail.com>
2020-05-26 16:28:43 +0200

    Restore and backup scripts updated

--- dba/db_restore.sql
+++ dba/db_restore.sql
@@ -19,3 +19,15 @@ MOVE N'WWI_Log'
 TO N'/var/opt/mssql/data/WideWorldImporters.ldf',  
 MOVE N'WWI_InMemory_Data_1' 
 TO N'/var/opt/mssql/data/WideWorldImporters_InMemory_Data_1';
+
+/*
+RESTORE DATABASE [AdventureWorks2017] 
+FROM DISK = N'/var/sqlbackup/AdventureWorks2017.bak' 
+WITH  NOUNLOAD, 
+REPLACE, -- if database is currently online we should use replace to restore it
+STATS = 5, -- NORECOVERY,
+MOVE N'AdventureWorks2017' 
+TO N'/var/opt/mssql/data/AdventureWorks2017.mdf',  
+MOVE N'AdventureWorks2017_log' 
+TO N'/var/opt/mssql/data/AdventureWorks2017_log.ldf'
+*/

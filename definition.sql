1785a17775cdd4307a20c7fc49a77f49b2a158fc
Ilia Odintsov <gofr.one@gmail.com>
2020-08-15 10:54:46 +0200

    Get table definition

--- definition.sql
+++ definition.sql
@@ -11,3 +11,20 @@ FROM sys.objects o
 INNER JOIN sys.syscomments c 
     ON o.[object_id] = c.id
 
+--Table definition
+SELECT OBJECT_SCHEMA_NAME(t.object_id) as schema_name,
+       OBJECT_NAME(t.object_id) as object_name,
+       c.name as column_name,
+       ty.name as type_name,
+       CASE WHEN ty.name IN ('nvarchar', 'nchar') THEN CONCAT('(', c.max_length/2, ')')
+            WHEN ty.name IN ('decimal', 'numeric') THEN CONCAT('(', c.[precision], ',', c.scale, ')')
+            WHEN ty.name IN ('varchar','char') THEN CONCAT('(', IIF(c.max_length = -1, 'max' ,LTRIM(STR(c.max_length))), ')')
+            ELSE '' END as size,
+        CASE WHEN c.is_nullable = 1 THEN 'NULL' ELSE 'NOT NULL' END as is_null
+        ,t.type
+FROM sys.tables t 
+INNER JOIN sys.columns c 
+    ON t.object_id = c.object_id
+INNER JOIN sys.types ty 
+    ON c.system_type_id = ty.system_type_id
+WHERE t.[type] = 'U'

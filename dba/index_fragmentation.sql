aebaeac368ab64dbb0f18ff6b956375da7fb454d
Ilia Odintsov <gofr.one@gmail.com>
2020-11-19 18:01:38 +0100

    Add info about rebuilding columnstore indexes

--- dba/index_fragmentation.sql
+++ dba/index_fragmentation.sql
@@ -3,7 +3,11 @@ USE DEMO;
 
 --sys.dm_db_index_physical_stats
 --avg_fragmentation_in_percent -> Logical fragmentation
+-- Logical fragmentation for indexes, or extent fragmentation for heaps in the IN_ROW_DATA allocation unit.
 --avg_page_space_used_in_percent -> Internal fragmentation
+
+--? External fragmentation – refers to pages being out of order
+--? Internal fragmentation – refers to the empty space on a page
 
 --!Reorganize when fragmentation 5-30%
 --!Rebuild when fragmentation > 30%
@@ -42,3 +46,23 @@ ALTER INDEX PK_IndexTest_id ON dbo.IndexTest REORGANIZE PARTITION = 1;
 ALTER INDEX ALL ON dbo.IndexTest REBUILD WITH (ONLINE=ON);
 ALTER INDEX PK_IndexTest_id ON dbo.IndexTest REBUILD WITH (ONLINE=ON);
 ALTER INDEX PK_IndexTest_id ON dbo.IndexTest REBUILD PARTITION = 1;
+
+--! Columnstore check
+--! >= 20% ALTER INDEX REORGANIZE
+
+--? total_rows 
+-- Number of rows physical stored in the row group. For compressed row groups, this includes the rows that are marked as deleted.
+--? deleted_rows 	
+-- Number of rows physically stored in a compressed row group that are marked for deletion. 0 for row groups that are in the delta store.
+SELECT i.object_id,
+       object_name(i.object_id) table_name,
+       i.index_id,
+       i.name index_name,
+       i.type_desc,
+       100*(ISNULL(SUM(c.deleted_rows),0))/NULLIF(SUM(c.total_rows),0) fragmentation
+FROM sys.indexes i  
+INNER JOIN sys.dm_db_column_store_row_group_physical_stats c
+    ON i.object_id = c.object_id AND i.index_id = c.index_id
+--WHERE object_name(i.object_id) = 'table_name'
+GROUP BY i.object_id, i.index_id, i.name, i.type_desc
+ORDER BY object_name(i.object_id), i.name;

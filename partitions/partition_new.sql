8e4b3dad888c94cbeb4915553e2828b5df0a8231
Ilia Odintsov <gofr.one@gmail.com>
2020-12-01 17:19:15 +0100

    Partition remove file

--- partitions/partition_new.sql
+++ partitions/partition_new.sql
@@ -1,26 +1,35 @@
 USE [master];
 
 --This script can be made with dynamic sql to automate new partition creation
-IF NOT EXISTS(SELECT 1 FROM DEMO.sys.filegroups WHERE name = 'test6fg')
-ALTER DATABASE DEMO ADD FileGroup test6fg;
+IF NOT EXISTS(SELECT 1 FROM DEMO.sys.filegroups WHERE name = 'test9fg')
+ALTER DATABASE DEMO ADD FileGroup test9fg;
 
-IF NOT EXISTS(SELECT 1 FROM DEMO.sys.database_files WHERE name = 'test6dat1')
+IF NOT EXISTS(SELECT 1 FROM DEMO.sys.database_files WHERE name = 'test9dat1')
 ALTER DATABASE DEMO 
 ADD FILE 
 (
-    NAME = test6dat1, 
-    FILENAME = '/var/opt/mssql/data/t6dat1.ndf', 
+    NAME = test9dat1, 
+    FILENAME = '/var/opt/mssql/data/t9dat1.ndf', 
     SIZE = 5MB, 
     MAXSIZE = 100MB,
     FILEGROWTH = 5MB 
 )
-TO FILEGROUP test6fg;
+TO FILEGROUP test9fg;
+
+-- Filegroup and respective files
+SELECT fg.[name],
+       df.[name]
+FROM DEMO.sys.filegroups fg
+INNER JOIN DEMO.sys.database_files df 
+    ON df.data_space_id = fg.data_space_id;
+
+-- Remove file that is not needed
+ALTER DATABASE DEMO REMOVE FILE test7dat1;
 
 USE DEMO;
 
-ALTER PARTITION SCHEME PS5000 NEXT USED test6fg;
-ALTER PARTITION FUNCTION PFdemo() SPLIT RANGE (20000);
-
+ALTER PARTITION SCHEME PS5000 NEXT USED test9fg;
+ALTER PARTITION FUNCTION PFdemo() SPLIT RANGE (35000);
 
 SELECT ps.Name AS PartitionScheme, 
        pf.name AS PartitionFunction,
@@ -41,4 +50,6 @@ LEFT JOIN sys.partition_range_values prv
 INNER JOIN sys.allocation_units au 
     ON au.container_id = p.hobt_id  
 INNER JOIN sys.filegroups fg 
-    ON fg.data_space_id = au.data_space_id 
+    ON fg.data_space_id = au.data_space_id
+ORDER BY PartitionScheme, PartitionFunction, IndexName, FileGroupName
+

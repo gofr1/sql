047639de007456c13262f4ba441e9ddded6a1f4f
Ilia Odintsov <gofr.one@gmail.com>
2020-12-01 17:21:59 +0100

    Some dataloads

---
+++ partitions/_load_data.sql
@@ -0,0 +1,24 @@
+USE DEMO;
+
+DROP TABLE IF EXISTS  dbo.IndexTestAddOn;
+
+WITH cte AS (
+    SELECT CONCAT(FirstName, ' ', LastName) as [Value]
+    FROM AdventureWorks2017.Person.Person
+    UNION ALL
+    SELECT AddressLine1
+    FROM AdventureWorks2017.Person.Address 
+)
+
+SELECT IDENTITY(int, 10001, 1) as Id,
+       [Value]
+INTO dbo.IndexTestAddOn
+FROM cte;
+
+SELECT MAX(Id) FROM dbo.IndexTest;
+
+INSERT INTO dbo.IndexTest
+SELECT *
+FROM dbo.IndexTestAddOn
+WHERE Id BETWEEN 10001 and 24568
+ORDER BY Id DESC

e704e23420f7ad2848d2905f2005bbe4fbbf980c
Ilia Odintsov <gofr.one@gmail.com>
2020-12-10 14:43:25 +0100

    Sargable queries explained

---
+++ performance/sargable.sql
@@ -0,0 +1,61 @@
+--! Sargable is a word that concatenates the three words: search, argument and able.
+-- 
+-- As per wikipedia SARGable is defined as “In relational databases, a condition (or predicate) in a query 
+-- is said to be sargable if the DBMS engine can take advantage of an index to speed up the execution of the query. 
+-- The term is derived from a contraction of Search ARGument ABLE”
+-- 
+-- Advantage of sargable queries include:
+--   consuming less system resources
+--   speeding up query performance
+--   using indexes more effectively
+
+--! In general you should not use functions on the columns in where statement
+
+USE DEMO;
+
+SET STATISTICS IO, TIME, PROFILE ON;
+
+SELECT * 
+FROM dbo.SearchARGumentABLE
+WHERE YEAR(OrderDate) = 2000 --! Index Scan
+
+--* SQL Server Execution Times:
+--* CPU time = 66 ms, elapsed time = 73 ms. 
+
+-- The SQL optimizer can't use an index on OrderDate, even if one exists. 
+-- It will literally have to evaluate this function for every row of the table. Much better to use:
+
+SELECT * 
+FROM dbo.SearchARGumentABLE
+WHERE OrderDate >= '01-01-2000' AND OrderDate < '01-01-2001' --? Index Seek
+	
+--* SQL Server Execution Times:
+--* CPU time = 4 ms, elapsed time = 3 ms. 
+
+-- Both will have almost same plan, but time of execution will be different
+--! bad way
+SELECT *
+FROM dbo.NCItest
+WHERE LEFT(SomeText,13) = 'Random Text 1'
+
+--* SQL Server Execution Times:
+--* CPU time = 67 ms, elapsed time = 71 ms.
+
+--? good way
+SELECT *
+FROM dbo.NCItest
+WHERE SomeText LIKE 'Random Text 1%'
+
+--* SQL Server Execution Times:
+--* CPU time = 31 ms, elapsed time = 31 ms. 
+ 
+
+
+
+
+--Bad: Select ... WHERE SUBSTRING(DealerName,4) = 'Ford'
+--Fixed: Select ... WHERE DealerName Like 'Ford%'
+
+--Bad: Select ... WHERE DateDiff(mm,OrderDate,GetDate()) >= 30
+--Fixed: Select ... WHERE OrderDate < DateAdd(mm,-30,GetDate()) 
+

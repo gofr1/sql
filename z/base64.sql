583a66abfc4a6316af67c263a3963bc673ab0dd4
Ilia Odintsov <gofr.one@gmail.com>
2020-12-14 11:57:25 +0100

    Base64 to string

--- z/base64.sql
+++ z/base64.sql
@@ -21,3 +21,10 @@ DECLARE @encodedString  VARCHAR(MAX) = 'give your html string that you want to e
         @base64 varbinary(max);
 
 SET @base64 = CAST(@encodedString as VARBINARY(MAX));
+
+-- From base64 encoded string to normal string
+SELECT CAST(N'' AS XML).value('xs:base64Binary(xs:hexBinary(sql:variable("@base64")))', 'VARCHAR(MAX)');
+
+DECLARE @Base64String varchar(max) = 'Z2l2ZSB5b3VyIGh0bWwgc3RyaW5nIHlvdSB3YW50IHRvIGVuY29kZQ==';
+
+SELECT CAST(CAST(N'' AS XML).value('xs:hexBinary(xs:base64Binary(sql:variable("@Base64String")))', 'VARBINARY(MAX)') as VARCHAR(MAX));

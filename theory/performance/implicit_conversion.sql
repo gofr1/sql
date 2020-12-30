865d24d276d4e601e48acb0a6f365a6421bfea60
Ilia Odintsov <gofr.one@gmail.com>
2020-12-30 17:30:40 +0300

    Monitor implicit conversions with extended events

--- theory/performance/implicit_conversion.sql
+++ theory/performance/implicit_conversion.sql
@@ -48,3 +48,76 @@ where IsFinalized LIKE '1%';
 -- Here we got 2 warnings
 
 SET STATISTICS PROFILE, XML OFF;
+
+-- To monitor implicit conversions we can use eXtended Events
+
+CREATE EVENT SESSION [ImplicitConversionCapture] ON SERVER 
+ADD EVENT sqlserver.plan_affecting_convert (
+    ACTION (
+        sqlserver.database_name
+    )
+    WHERE (
+        sqlserver.database_name = N'WideWorldImporters'
+    )
+),
+ADD EVENT sqlserver.sql_batch_completed (
+    ACTION (
+        sqlserver.database_name
+    )
+    WHERE (
+        sqlserver.database_name = N'WideWorldImporters'
+    )
+),
+ADD EVENT sqlserver.sql_batch_starting (
+    ACTION (
+        sqlserver.database_name
+    )
+    WHERE (
+        sqlserver.database_name = N'WideWorldImporters'))
+ADD TARGET package0.event_file (
+    SET filename = N'/var/log/ImplicitConversion.xel'
+)
+WITH (
+    MAX_MEMORY=4096 KB,
+    EVENT_RETENTION_MODE=ALLOW_SINGLE_EVENT_LOSS,
+    MAX_DISPATCH_LATENCY=30 SECONDS,
+    MAX_EVENT_SIZE=0 KB,
+    MEMORY_PARTITION_MODE=NONE,
+    TRACK_CAUSALITY=OFF,
+    STARTUP_STATE=OFF
+)
+GO
+
+ALTER EVENT SESSION [ImplicitConversionCapture] 
+ON SERVER STATE = START;
+GO
+
+
+SELECT TransactionDate,
+       IsFinalized
+FROM [Sales].[CustomerTransactions]
+where IsFinalized LIKE '1%';
+
+
+ALTER EVENT SESSION ImplicitConversionCapture
+ON SERVER STATE = STOP;
+GO
+
+DROP EVENT SESSION ImplicitConversionCapture ON SERVER;
+GO
+
+-- Now we shall take a look on results
+SELECT --[XMLData],
+       [XMLData].value('(/event/@name)[1]','varchar(max)') AS [EventName],
+       [XMLData].value('(/event/@timestamp)[1]','DATETIME2') AS [Timestamp],
+       [XMLData].value('(/event/data[@name=''batch_text'']/value)[1]','varchar(max)') AS [BatchText],
+       [XMLData].value('(/event/data[@name=''expression'']/value)[1]','varchar(max)') AS [Expression],
+       [XMLData].value('(/event/data[@name=''convert_issue'']/text)[1]','varchar(max)') AS [ConvertIssue]
+FROM (
+    SELECT OBJECT_NAME              AS [Event], 
+           CONVERT(XML, event_data) AS [XMLData]
+    FROM sys.fn_xe_file_target_read_file ('/var/log/ImplicitConversion*.xel',NULL,NULL,NULL)
+) as me
+WHERE [XMLData].value('(/event/action[@name=''database_name'']/value)[1]','varchar(max)') = N'WideWorldImporters'
+ORDER BY [Timestamp];
+GO

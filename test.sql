e2aaa39e7b52fc33de5ff994bc136c79fa67c85b
Ilia Odintsov <gofr.one@gmail.com>
2021-01-10 18:12:27 +0300

    Check database encryption

--- test.sql
+++ test.sql
@@ -66,3 +66,14 @@ SELECT SERVERPROPERTY('Collation') Collation,
   
 SELECT *  
 FROM sys.database_scoped_configurations;
+
+-- DB encryption keys
+SELECT db.name,
+       db.is_encrypted,
+       dm.encryption_state,
+       dm.percent_complete,
+       dm.key_algorithm,
+       dm.key_length
+FROM sys.databases db
+LEFT JOIN sys.dm_database_encryption_keys dm
+    ON db.database_id = dm.database_id;

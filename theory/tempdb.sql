1bf083ad1f8f502d56074640c66f1876a99f9a8d
Ilia Odintsov <gofr.one@gmail.com>
2021-01-22 16:54:38 +0300

    tempdb: working with files & checking DB

--- theory/tempdb.sql
+++ theory/tempdb.sql
@@ -12,4 +12,44 @@
 -- Any objects that have been created in tempdb during a previous session will not persist upon a service restart. 
 -- tempdb gets its initial object list from the model database which is generally going to be empty or nearly empty.
 
+--! There should be one tempdb data file for each thread/core/vCPU on the instance with a maximum of 8
+USE [master];
+
+ALTER DATABASE tempdb ADD FILE (NAME = N'temp2', FILENAME = N'/var/opt/mssql/data/tempdb2.ndf', SIZE = 8192KB);
+ALTER DATABASE tempdb ADD FILE (NAME = N'temp3', FILENAME = N'/var/opt/mssql/data/tempdb3.ndf', SIZE = 8192KB);
+ALTER DATABASE tempdb ADD FILE (NAME = N'temp4', FILENAME = N'/var/opt/mssql/data/tempdb4.ndf', SIZE = 8192KB);
+
+-- In the terminal:
+-- sudo ls -lah /var/opt/mssql/data/ | grep temp
+--* -rw-rw----  1 mssql mssql 8.0M Jan 22 15:47 tempdb2.ndf
+--* -rw-rw----  1 mssql mssql 8.0M Jan 22 15:47 tempdb3.ndf
+--* -rw-rw----  1 mssql mssql 8.0M Jan 22 15:47 tempdb4.ndf
+--* -rw-r-----  1 mssql mssql 8.0M Jan 22 15:47 tempdb.mdf
+--* -rw-r-----  1 mssql mssql 8.0M Jan 22 15:48 templog.ldf
+
+--! After resizing/adding/removing files restart SQL Server
+
+-- USE [master];
+-- ALTER DATABASE tempdb REMOVE FILE temp2;
+-- ALTER DATABASE tempdb REMOVE FILE temp3;
+-- ALTER DATABASE tempdb REMOVE FILE temp4;
+
+USE [tempdb];
+--! SQL Server tempdb Database Location
+-- To get info about tempdb files use:
+SELECT *
+FROM sys.sysfiles;
+
+SELECT [name], 
+       physical_name,
+       state_desc
+FROM [master].sys.master_files
+WHERE database_id = DB_ID(N'tempdb');
+
+--! Moving/Modifying tempdb files
+ALTER DATABASE [tempdb] MODIFY FILE ( NAME = N'tempdev', FILENAME = N'/var/opt/mssql/data/tempdb.mdf');
+ALTER DATABASE [tempdb] MODIFY FILE ( NAME = N'templog', FILENAME = N'/var/opt/mssql/data/templog.ldf');
+
+--! Run an integrity check is below.
+DBCC CHECKDB ('tempdb'); 
 
